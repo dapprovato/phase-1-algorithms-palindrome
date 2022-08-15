2b2fc7f5c7aaff80ac9b4c34f734f9517f43e7ce
Daria Approvato <dariaapprovato@gmail.com>
2022-08-14 21:36:38 -0400

    Completed Palindrome Lab

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let splitWord =  word.split("")
+  let reverseWord = splitWord.reverse()
+  let joinWord = reverseWord.join("")
+
+  if (joinWord === word) {
+    return true
+  } else return false
 }
 
 /* 
-  Add your pseudocode here
+  Add your pseudocode here :
+  initialize function
+  check possible applicable syntax , needing to reverse a string
+  turn string to array , reverse array , join back together!
+  if stmnt for T or F
 */
 
 /*
-  Add written explanation of your solution here
+  Add written explanation of your solution here :
+  initialize isPalindrome
+  use .split to separate letters of 'word' string + turn into array
+  use .reverse on array to reverse letters
+  use .join to put array back into string
+  if / else stmnt to return true or false
 */
 
 // You can run `node index.js` to view these console logs
